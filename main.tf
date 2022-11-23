6d173c72126c17a16fcee18293e40acdbb8a2fe2
Jonathan Lefebvre <jonathan.lef@gmail.com>
2022-11-23 11:33:15 -0500

    add provider

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 module "vpc" {
-  source              = "./modules/vpc"
+  source = "./modules/vpc"
+  providers = {
+    google = google.target
+  }
   prefix              = var.prefix
   private_subnet_cidr = "10.0.0.0/24"
   public_subnet_cidr  = "10.1.0.0/24"
@@ -7,6 +10,9 @@ module "vpc" {
 
 module "http_server" {
   source = "./modules/http_server"
+  providers = {
+    google = google.target
+  }
   prefix = var.prefix
   zone   = "${var.region}-a"
   subnet = module.vpc.private_subnet_name
@@ -14,19 +20,28 @@ module "http_server" {
 
 module "bastion" {
   source = "./modules/bastion"
+  providers = {
+    google = google.target
+  }
   prefix = var.prefix
   zone   = "${var.region}-a"
   subnet = module.vpc.public_subnet_name
 }
 
 module "firewall" {
-  source   = "./modules/firewall"
+  source = "./modules/firewall"
+  providers = {
+    google = google.target
+  }
   prefix   = var.prefix
   vpc_name = module.vpc.vpc_name
 }
 
 module "nat" {
-  source   = "./modules/nat"
+  source = "./modules/nat"
+  providers = {
+    google = google.target
+  }
   prefix   = var.prefix
   vpc_name = module.vpc.vpc_name
   region   = var.region
